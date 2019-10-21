b411806e9d534d5b918f74c0164e8390ce1b7b5c
EagleEye25 <jasonheybers@gmail.com>
2019-10-21 10:20:11 +0200

    hide input

--- src/components/clocker.vue
+++ src/components/clocker.vue
@@ -4,11 +4,11 @@
     <div v-if="action === 'Clock_In' || action === null || action === 'completed'">
       <img class="center" src="../../public/pictures/lock.png" alt="Lock Image">
       <!-- Hides input off screen -->
-      <!-- <div class="outer"> -->
-        <!-- <div class="inner"> -->
+      <div class="outer">
+        <div class="inner">
           <input v-focus v-model="tag" type="text" @keyup.enter="onEnter" id="cardInput">
-        <!-- </div> -->
-      <!-- </div> -->
+        </div>
+      </div>
       <h1 style="color:white;">{{ message }}</h1>
     </div>
     <div v-if="action === 'Clock_Out'" class="center">
